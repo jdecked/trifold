51cc48ecf8a112afee55418b1c4f904da2c714a5
Justine De Caires <justine@minerva.kgi.edu>
2019-02-11 21:33:40 -0800

    hahaha

--- docker_build.sh
+++ docker_build.sh
@@ -3,9 +3,13 @@
 echo $TRAVIS_BRANCH
 
 docker login -u $DOCKER_ID --password-stdin $DOCKER_PASSWORD
+echo 'still going 1'
 docker pull $DOCKER_ID/$API
+echo 'still going 2'
 docker pull $DOCKER_ID/$CLIENT
+echo 'still going 3'
 docker pull $DOCKER_ID/$NGINX
+echo 'still going 4'
 
 docker-compose build
 docker-compose -f docker-compose-ci.yml up -d --build
