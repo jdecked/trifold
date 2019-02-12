267a24a20b3b1a135369ad1355304a6be6bf1cb8
Justine De Caires <justine@minerva.kgi.edu>
2019-02-11 21:20:02 -0800

    We try

--- docker_build.sh
+++ docker_build.sh
@@ -2,12 +2,10 @@
 
 echo $TRAVIS_BRANCH
 
-if [ "$TRAVIS_BRANCH" == "development" ]; then
-  docker login -u $DOCKER_ID -p $DOCKER_PASSWORD
-  docker pull $DOCKER_ID/$API
-  docker pull $DOCKER_ID/$CLIENT
-  docker pull $DOCKER_ID/$NGINX
-fi
+docker login -u $DOCKER_ID -p $DOCKER_PASSWORD
+docker pull $DOCKER_ID/$API
+docker pull $DOCKER_ID/$CLIENT
+docker pull $DOCKER_ID/$NGINX
 
 docker-compose build
 docker-compose -f docker-compose-ci.yml up -d --build
