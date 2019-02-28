47d882d666e284822f171c00b7f54ab060a58d46
Justine De Caires <justine@minerva.kgi.edu>
2019-02-27 21:19:40 -0800

    Sweet, sweet victory

--- docker_push.sh
+++ docker_push.sh
@@ -8,23 +8,26 @@ then
     docker login -e $DOCKER_EMAIL -u $DOCKER_ID -p $DOCKER_PASSWORD
     export TAG=$TRAVIS_BRANCH
     export REPO=$DOCKER_ID
-    export HTTPS=true
     export DJANGO_ENVIRONMENT="development"
     export DJANGO_SECRET_KEY="everything_is_tater_tots"
+    export REACT_APP_OAUTH_CLIENT_ID=$REACT_APP_OAUTH_CLIENT_ID
+    export CLIENT_ID=$CLIENT_ID
   fi
 
   if [ "$TRAVIS_BRANCH" == "staging" ]
   then
-    export HTTPS=true
     export DJANGO_ENVIRONMENT="production"
     export DJANGO_SECRET_KEY="$DJANGO_SECRET_KEY"
+    export REACT_APP_OAUTH_CLIENT_ID=$REACT_APP_OAUTH_CLIENT_ID
+    export CLIENT_ID=$CLIENT_ID
   fi
 
   if [ "$TRAVIS_BRANCH" == "production" ]
   then
-    export HTTPS=true
     export DJANGO_ENVIRONMENT="production"
     export DJANGO_SECRET_KEY="$DJANGO_SECRET_KEY"
+    export REACT_APP_OAUTH_CLIENT_ID=$REACT_APP_OAUTH_CLIENT_ID
+    export CLIENT_ID=$CLIENT_ID
   fi
 
   if [ "$TRAVIS_BRANCH" == "development" ] || \
@@ -32,19 +35,21 @@ then
      [ "$TRAVIS_BRANCH" == "production" ]
   then
     # api
-    if [ "$TRAVIS_BRANCH" == "production" ]
+    if [ "$TRAVIS_BRANCH" == "production" ] || \
+       [ "$TRAVIS_BRANCH" == "staging" ]
     then
-      docker build $API_REPO -t $API:$COMMIT -f Dockerfile-prod
-    else
       docker build $API_REPO -t $API:$COMMIT
+      docker build $CLIENT_REPO -t $CLIENT:$COMMIT
+    else
+      docker build $API_REPO -t $API:$COMMIT -f Dockerfile-local
+      docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-local
     fi
     docker tag $API:$COMMIT $REPO/$API:$TAG
     docker push $REPO/$API:$TAG
-    # client
-    docker build $CLIENT_REPO -t $CLIENT:$COMMIT
+
     docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
     docker push $REPO/$CLIENT:$TAG
-    # nginx
+
     docker build $NGINX_REPO -t $NGINX:$COMMIT
     docker tag $NGINX:$COMMIT $REPO/$NGINX:$TAG
     docker push $REPO/$NGINX:$TAG
