0e05776cc446abc366ac5f64b0b6d26e42b17bea
Justine De Caires <justine@minerva.kgi.edu>
2019-03-02 21:51:43 -0800

    Now to fix the heroku build

--- docker_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_ID --password-stdin
-docker pull $DOCKER_ID/$API
-docker pull $DOCKER_ID/$CLIENT
-docker pull $DOCKER_ID/$NGINX
-
-docker-compose build
-docker-compose -f docker-compose-ci.yml up -d --build
